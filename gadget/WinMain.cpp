d466803cf3f21d3fcffa85347611cf831cdf460a
Nankotsukan <Nankotsukan@NANKOTSUKANSPC>
2017-08-06 01:56:00 +0900

    clock

---
+++ gadget/WinMain.cpp
@@ -0,0 +1,113 @@
+﻿
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#include"show.h"
+#include<time.h>
+
+POINT  pt = { 5, 5 };
+HINSTANCE hInst;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+ static TCHAR szWindowClass[] = _T("gadget");
+
+ WNDCLASSEX wcex;
+
+ wcex.cbSize = sizeof(WNDCLASSEX);
+ wcex.style = CS_HREDRAW | CS_VREDRAW;
+ wcex.lpfnWndProc = WndProc;
+ wcex.cbClsExtra = 0;
+ wcex.cbWndExtra = 0;
+ wcex.hInstance = hInstance;
+ wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+ wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+ wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+ wcex.lpszMenuName = NULL;
+ wcex.lpszClassName = szWindowClass;
+ wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+ if (!RegisterClassEx(&wcex))
+ {
+  MessageBox(NULL,
+   _T("error"),
+   _T("gadget"),
+   NULL);
+
+  return 1;
+ }
+
+ hInst = hInstance; 
+ HWND hWnd = CreateWindowEx(WS_EX_TOOLWINDOW | WS_EX_LAYERED | /*WS_EX_TRANSPARENT |*/ WS_EX_TOPMOST,
+  szWindowClass,
+  _T(""), WS_POPUP | WS_SYSMENU | WS_VISIBLE,
+  1500, 0, 420, 1080,
+  NULL, NULL, hInstance, NULL);
+ if (!hWnd)
+ {
+  MessageBox(NULL,
+   _T("ウィンドウ生成に失敗しました!"),
+   _T("Sample02"),
+   NULL);
+  return 1;
+ }
+
+ SetLayeredWindowAttributes(hWnd, RGB(255,255,255), 10, LWA_COLORKEY);
+ //SetLayeredWindowAttributes(hWnd, RGB(255, 0, 0), 10, LWA_COLORKEY);
+
+ ShowWindow(hWnd,
+  nCmdShow);
+ UpdateWindow(hWnd);
+
+ MSG msg;
+ while (GetMessage(&msg, NULL, 0, 0))
+ {
+  TranslateMessage(&msg);
+  DispatchMessage(&msg);
+ }
+ return (int)msg.wParam;
+}
+
+
+int i = 0;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+ HDC hdc;
+ PAINTSTRUCT ps;
+ TCHAR   szstr[256];
+
+ switch (message) {
+ case WM_CREATE:
+  SetTimer(hWnd, 1, 1000, NULL);
+  break;
+ case WM_TIMER:
+  _stprintf_s(szstr, _T("%s"), "mazai");
+  InvalidateRect(hWnd, NULL, TRUE);
+
+
+  break;
+ case WM_DESTROY:
+  KillTimer(hWnd, 1);
+  PostQuitMessage(0);
+  return 0;
+ case WM_PAINT:
+  time_t t = time(NULL);
+  _stprintf_s(szstr, _T("%s"), ctime(&t));
+  hdc = BeginPaint(hWnd, &ps);
+  //_stprintf_s(szstr, _T("CPU使用率は%4uです"), cpuRatio[0]);
+  TextOut(hdc,
+   5, 5,
+   szstr, _tcslen(szstr)-1);
+  EndPaint(hWnd, &ps);
+
+  break;
+
+  return 0;
+ }
+ return DefWindowProc(hWnd, message, wParam, lParam);
+}
---
+++ gadget/show.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <windows.h>
+#include <stdlib.h>
+#include <string.h>
+#include <tchar.h>
+#include <conio.h>
+#include <pdh.h>
+#include <deque>
+#include<time.h>
+#pragma comment(lib, "pdh.lib")
+
+
+class show
+{
+public:
+ show();
+ ~show();
+ void showCpu(HWND hWnd);
+
+private:
+ int ram;
+ int core;
+ PDH_HQUERY hQuery[17];
+ PDH_HCOUNTER hCounter[17];
+ PDH_FMT_COUNTERVALUE fntValue;
+ std::deque<char> cpuRatio;
+
+
+
+};
+
+show::show()
+{
+ MEMORYSTATUSEX msex = { sizeof(MEMORYSTATUSEX) };
+ GlobalMemoryStatusEx(&msex);
+
+ int n;
+ char query[128];
+ 
+
+ core = atoi(getenv("NUMBER_OF_PROCESSORS"));
+ for (n = 0; n < core + 1; n++) {
+  PdhOpenQuery(NULL, 0, &hQuery[n]);
+ }
+
+ for (n = 0; n < core; n++) {
+  PdhAddCounter(hQuery[n], query, 0, &hCounter[n]);
+  PdhCollectQueryData(hQuery[n]);
+ }
+ PdhAddCounter(hQuery[core], ("\\Processor(_Total)\\% Processor Time"), 0, &hCounter[core]);
+ PdhCollectQueryData(hQuery[core]);
+}
+
+show::~show()
+{
+}
+
+void show::showCpu(HWND hWnd) {
+ for (int n = core; n < core + 1; n++) {
+  //Sleep(1000);
+  PdhCollectQueryData(hQuery[n]);
+  PdhGetFormattedCounterValue(hCounter[n], PDH_FMT_LONG, NULL, &fntValue);
+  cpuRatio.push_front(fntValue.longValue);//CPU使用率
+  
+  time_t t = time(NULL);
+
+
+  HDC hdc;
+  PAINTSTRUCT ps;
+
+  hdc = BeginPaint(hWnd, &ps);
+  TCHAR   szstr[256];
+  _stprintf_s(szstr, _T("CPU使用率は%4uです"), cpuRatio[0]);
+  //_stprintf_s(szstr, _T("%s"),  ctime(&t));
+  TextOut(hdc,
+   5, 5,
+   szstr, _tcslen(szstr));
+  EndPaint(hWnd, &ps);
+
+
+  hdc = BeginPaint(hWnd, &ps);
+  HPEN        hPen, hOldPen;
+  hPen = CreatePen(PS_SOLID, 1, RGB(0, 200, 150));
+  hOldPen = (HPEN)SelectObject(hdc, hPen);
+  MoveToEx(hdc, 50, 50, NULL);
+  LineTo(hdc, 50, 200);
+  LineTo(hdc, 350, 200);
+  LineTo(hdc, 350, 50);
+  LineTo(hdc, 50, 50);
+  if (cpuRatio.size() > 100)cpuRatio.pop_back();
+  hPen = CreatePen(PS_SOLID, 20, RGB(0, 200, 150));
+  hOldPen = (HPEN)SelectObject(hdc, hPen);
+  MoveToEx(hdc, 50+100-cpuRatio.back(),300, NULL);
+  for (int i = cpuRatio.size() - 1;i>0;--i) {
+  LineTo(hdc, 50 + 100 - cpuRatio[i]+10, 350-i);
+  }
+ }
+}
