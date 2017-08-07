90121eaa52c45465c7e1900e2124b2d25f161522
Nankotsukan <Nankotsukan@NANKOTSUKANSPC>
2017-08-07 23:36:05 +0900

    CPU計の実装 全コアの使用率を表示

--- gadget/show.h
+++ gadget/show.h
@@ -8,6 +8,7 @@
 #include <pdh.h>
 #include <deque>
 #include<time.h>
+#include<vector>
 #pragma comment(lib, "pdh.lib")
 
     
@@ -18,6 +19,7 @@ public:
     ~show();
     void showCpu(HWND, HDC);
     void showTime(HWND,HDC );
+    void showMem(HWND,HDC);
     void test(HWND, HDC);
 private:
     int ram;
@@ -25,13 +27,10 @@ private:
     PDH_HQUERY hQuery[17];
     PDH_HCOUNTER hCounter[17];
     PDH_FMT_COUNTERVALUE fntValue;
-    std::deque<char> cpuRatio;
+    std::vector<std::deque<char>> cpuRatio;
     HPEN hPenG2 = CreatePen(PS_SOLID, 1, RGB(0, 220, 150));
-    HPEN hPenG = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
-
-
-
-
+    HPEN hPenG = CreatePen(PS_SOLID, 1, RGB(0, 255, 255));
+    std::vector<HPEN> col;
 
 };
 
@@ -42,17 +41,26 @@ show::show()
 
     int n;
     char query[128];
- 
+    
 
     core = atoi(getenv("NUMBER_OF_PROCESSORS"));
     for (n = 0; n < core + 1; n++) {
         PdhOpenQuery(NULL, 0, &hQuery[n]);
     }
 
-    for (n = 0; n < core; n++) {
+    for (n = 0; n < core+1; n++) {
+        sprintf(query, "\\Processor(%d)\\%% Processor Time", n);
         PdhAddCounter(hQuery[n], query, 0, &hCounter[n]);
         PdhCollectQueryData(hQuery[n]);
     }
+    std::deque<char> a(300,0);
+    for ( n = 0; n < core+1; n++)
+    {
+        cpuRatio.push_back(a);
+        col.push_back(CreatePen(PS_SOLID, 1, RGB(0, 256 - n*32,n*32)));
+    }
+    col[core]= CreatePen(PS_SOLID, 1, RGB(255,0,0));
+
     PdhAddCounter(hQuery[core], ("\\Processor(_Total)\\% Processor Time"), 0, &hCounter[core]);
     PdhCollectQueryData(hQuery[core]);
 }
@@ -64,24 +72,26 @@ show::~show()
 void show::showCpu(HWND hWnd, HDC hdc) {
     //hPen = CreatePen(PS_SOLID, 1, RGB(0, 255, 0));
     SelectObject(hdc, hPenG);
+    MoveToEx(hdc, 50, 50, NULL);
+    LineTo(hdc, 50, 150);
+    LineTo(hdc, 350, 150);
+    LineTo(hdc, 350, 50);
+    LineTo(hdc, 50, 50);
 
-    for (int n = core; n < core + 1; n++) {
+    for (int n = 0; n < core + 1; ++n) {
         //Sleep(1000);
-        PdhCollectQueryData(hQuery[n]);
-        PdhGetFormattedCounterValue(hCounter[n], PDH_FMT_LONG, NULL, &fntValue);
-        cpuRatio.push_front(fntValue.longValue);//CPU使用率
- 
-        PAINTSTRUCT ps;
+        cpuRatio[n].pop_back();
 
         //HPEN        hPen;
         //hPen = CreatePen(PS_SOLID, 10, RGB(0, 200, 150));
         //hOldPen = (HPEN)SelectObject(hdc, hPen);
-        MoveToEx(hdc, 50, 50, NULL);
-        LineTo(hdc, 50, 200);
-        LineTo(hdc, 350, 200); 
-        LineTo(hdc, 350, 50);
-        LineTo(hdc, 50, 50);
-        if (cpuRatio.size() > 300)cpuRatio.pop_back()   ;
+
+        PdhCollectQueryData(hQuery[n]);
+        PdhGetFormattedCounterValue(hCounter[n], PDH_FMT_LONG, NULL, &fntValue);
+        cpuRatio[n].push_front(fntValue.longValue);//CPU使用率
+        //char temp= fntValue.longValue;
+        //temp;
+        //cpuRatio[n].push_front(temp);
         //hPen = CreatePen(PS_SOLID, 20, RGB(0, 200, 150));
         //hOldPen = (HPEN)SelectObject(hdc, hPen);
         //MoveToEx(hdc,350, 50+100-cpuRatio.back(), NULL);
@@ -90,15 +100,13 @@ void show::showCpu(HWND hWnd, HDC hdc) {
         //    LineTo(hdc,350 - i,50 + 100 - cpuRatio[i]+10);
         //}
 
-        SelectObject(hdc, hPenG2);
+        SelectObject(hdc, col[n]);
 
-        MoveToEx(hdc, 350- cpuRatio.size(), 50 + 100 - cpuRatio.back(), NULL);
+        MoveToEx(hdc, 350- cpuRatio[n].size(), 50 + 100 - cpuRatio[n].back(), NULL);
 
-        for (int i = cpuRatio.size() - 1;i>0;--i) {
-            LineTo(hdc, 350 - i, 50 + 100 - cpuRatio[i]);
+        for (int i = 300;i>0;--i) {
+            LineTo(hdc, 350 - i, 50 + 100 - cpuRatio[n][i]);
         }
-        EndPaint(hWnd, &ps);
-
         //InvalidateRect(hWnd, NULL, TRUE);
 
         //TCHAR   szstr[256];
@@ -112,7 +120,7 @@ void show::showTime(HWND hWnd, HDC hdc) {
     //LOGFONT lfFont;
     //lfFont.lfHeight = 10;
     //lfFont.lfWeight = FW_EXTRALIGHT;
-
+    auto col=RGB(0,0,0);
     PAINTSTRUCT ps;
     TCHAR   szstr[256];
     time_t t = time(NULL);
@@ -125,6 +133,10 @@ void show::showTime(HWND hWnd, HDC hdc) {
     //SelectObject(hdc, hFont);
 
     TextOut(hdc, 10, 5, szstr, _tcslen(szstr) - 1);
+}
+
+void show::showMem(HWND hWnd, HDC hdc) {
+
 }
 
 void show::test(HWND hWnd, HDC hdc) {
